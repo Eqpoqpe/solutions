e9ed5c1eb13a59224dfc1e4e11f3cef0dc0832e0
Ryan Martin <54093748+eqpoqpe@users.noreply.github.com>
2021-08-11 20:30:36 +0800

    Update unit-movezeroes.sh

--- bin/unit-movezeroes.sh
+++ bin/unit-movezeroes.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+declare readonly WORKING_DIR=$(pwd)
+declare readonly USER_DIR=$HOME
+
+cat <<EOF > /tmp/movezeroes.c
+#include <stdio.h>
+int main()
+{
+    int nums_0[7] = {1,2,0,0,2,0,1};
+    return 0;
+}
+EOF
+
+gcc -o /tmp/movezeroes /tmp/movezeroes.c $WORKING_DIR/movezeroes.c
+if [ $? -eq 0 ]; then
+    /tmp/movezeroes
+fi
+
+rm /tmp/movezeroes.c /tmp/movezeroes
