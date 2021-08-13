4981611980929120a6f2cc6ff89a310b219d8865
Ryan Martin <54093748+eqpoqpe@users.noreply.github.com>
2021-08-13 11:14:19 +0800

    Update unit-rotate.sh

--- bin/unit-rotate.sh
+++ bin/unit-rotate.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+declare readonly WORKING_DIR=$(pwd)
+declare readonly USER_DIR=$HOME
+
+cat <<EOF > /tmp/unit-rotate.c
+#include <stdio.h>
+int main()
+{
+    int nums[5] = {3,2,5,0,7};
+    printf("Original: %d\n", nums[0]);
+    naive_rotate(nums, 5, 100);
+    printf("Result: %d\n", nums[0]);
+    return 0;
+}
+EOF
+
+gcc -o /tmp/rotate /tmp/unit-rotate.c $WORKING_DIR/rotate.c
+if [ $? -eq 0 ]; then
+    /tmp/rotate
+fi
+
+rm /tmp/unit-rotate.c /tmp/rotate
